b8710ca2c62a652ce5ea877f1ae89ad02eb5a136
Ian Mitchell <ian.mitchel1@live.com>
2014-01-18 10:36:43 -0800

    Added Roles to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: {
+    person: 0,
+    organization: 1,
+    admin: 2
+  }
+
 end

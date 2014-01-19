b36b8073451bec995c1a408b3fe00d64cdc46f68
bmmoore94 <bm.moore94@gmail.com>
2014-01-19 09:20:16 -0800

    connecting posts!!!!!

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,18 @@ class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def connect
+  	@post = Post.find(params[:id])
+
+  	if @post.user 
+  		@post.buyer=current_user
+  	else
+  		@post.user=current_user
+  	end
+
+  	@post.staus= :completed
+
+  	redirect_to dashboard_show_path
+  end
 end

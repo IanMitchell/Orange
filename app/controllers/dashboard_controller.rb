828571cfb536d09bdcb97ef2677fc3985ae99e0e
bmmoore94 <bm.moore94@gmail.com>
2014-01-19 12:43:33 -0800

    maybe?????

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,27 +3,16 @@ class DashboardController < ApplicationController
 
   def show
     if params[:filter] == "open"
-<<<<<<< HEAD
-      @posts = Post.where(created_by: current_user).open
+      @posts = Post.where(created_by: current_user).open.order('created_at DESC')
       #@posts = current_user.posts.open
       #@posts.zip(Post.where(buyer: current_user).open).flatten.compact      
    elsif params[:filter] == "completed"
       #@posts = current_user.posts.completed
-      @posts = Post.where(created_by: current_user).completed
+      @posts = Post.where(created_by: current_user).completed.completedorder('created_at DESC')
       #@posts.zip(Post.where(buyer: current_user).completed).flatten.compact      
     elsif params[:filter] == "all"
-      @posts = Post.where(created_by: current_user)
+      @posts = Post.where(created_by: current_user).order('created_at DESC')
       #@posts.zip(Post.where(created_by: current_user)).flatten.compact      
-=======
-      @posts = current_user.posts.openorder('created_at DESC')
-      @posts.zip(Post.where(buyer: current_user).open.order('created_at DESC')).flatten.compact
-   elsif params[:filter] == "completed"
-      @posts = current_user.posts.completedorder('created_at DESC')
-      @posts.zip(Post.where(buyer: current_user).completed.order('created_at DESC')).flatten.compact
-    elsif params[:filter] == "all"
-      @posts = current_user.posts.order('created_at DESC')
-      @posts.zip(Post.where(buyer: current_user).order('created_at DESC')).flatten.compact
->>>>>>> e98a5e4533c7a9f2e571f00cb5b2c19be4bcc134
     else
       @posts = Post.all.order('created_at DESC')
     end

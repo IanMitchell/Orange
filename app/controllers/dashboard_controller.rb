5d502f47bea77ed00e7666bd99c143b69276673e
Ian Mitchell <ian.mitchel1@live.com>
2014-01-19 14:18:35 -0800

    Order Fix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,16 +3,16 @@ class DashboardController < ApplicationController
 
   def show
     if params[:filter] == "open"
-      @posts = Post.where(created_by: current_user).open.order('created_at ASC')
+      @posts = Post.where(created_by: current_user).open.order('created_at DESC')
       #@posts = current_user.posts.open
-      #@posts.zip(Post.where(buyer: current_user).open).flatten.compact      
+      #@posts.zip(Post.where(buyer: current_user).open).flatten.compact
    elsif params[:filter] == "completed"
       #@posts = current_user.posts.completed
-      @posts = Post.where(created_by: current_user).completed.completedorder('created_at ASC')
-      #@posts.zip(Post.where(buyer: current_user).completed).flatten.compact      
+      @posts = Post.where(created_by: current_user).completed.order('created_at DESC')
+      #@posts.zip(Post.where(buyer: current_user).completed).flatten.compact
     elsif params[:filter] == "all"
-      @posts = Post.where(created_by: current_user).order('created_at ASC')
-      #@posts.zip(Post.where(created_by: current_user)).flatten.compact      
+      @posts = Post.where(created_by: current_user).order('created_at DESC')
+      #@posts.zip(Post.where(created_by: current_user)).flatten.compact
     else
       @posts = Post.all.order('created_at DESC')
     end

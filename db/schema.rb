b0477afd2f86c0f609e1ad95d01a4df48ea20f19
Ian Mitchell <ian.mitchel1@live.com>
2014-01-19 15:47:11 -0800

    Added User Description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140119191253) do
+ActiveRecord::Schema.define(version: 20140119234536) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20140119191253) do
     t.string   "twitter"
     t.string   "facebook"
     t.string   "phone"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

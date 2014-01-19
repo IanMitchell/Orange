ba6a17bfccb48aff9b314e556220dc2581ae785c
Sunjay Dhama <sdsunjay73@yahoo.com>
2014-01-19 19:04:48 +0000

    fixed the open and completed posts thing

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,10 +4,13 @@ class DashboardController < ApplicationController
   def show
     if params[:filter] == "open"
       @posts = current_user.posts.open
-    elsif params[:filter] == "completed"
+      @posts.zip(Post.where(buyer: current_user).open).flatten.compact      
+   elsif params[:filter] == "completed"
       @posts = current_user.posts.completed
+      @posts.zip(Post.where(buyer: current_user).completed).flatten.compact      
     elsif params[:filter] == "all"
       @posts = current_user.posts
+      @posts.zip(Post.where(buyer: current_user)).flatten.compact      
     else
       @posts = Post.all
     end

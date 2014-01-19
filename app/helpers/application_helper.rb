37816d8e2f1aba5c81379447b9a898f10a86c28f
bmmoore94 <bm.moore94@gmail.com>
2014-01-18 19:42:35 -0800

    fixed gravatar fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
 	def get_gravatar(user,size=150)
-	 "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}.jpg?s={size}"
+	 "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}.jpg?s=#{size}"
 	end
 end

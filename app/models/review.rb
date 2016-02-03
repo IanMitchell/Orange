0af7643360c362f308657bb17997e76152391afb
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-02-02 23:30:22 -0800

    added title for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,12 +4,16 @@ class Review < ActiveRecord::Base
   belongs_to :owner, class_name: 'user'
 
 
+  validates :user_id,  presence: true
+  validates :owner_id,  presence: true
+  validates :score,  presence: true
   validates :feedback,  presence: true,
                         length: { in: 3..256 }
+  validates :title,  presence: true,length: { in: 3..128 }
 
-  enum score: {
-    neutral: 0,
-    negative: -1,
-    positive: 1
-  }
+  #enum score: {
+  #  neutral: 0,
+  #  negative: -1,
+  #  positive: 1
+  #}
 end
